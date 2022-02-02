2ab74df66b35a31ad5b09e27c475999df0e4c78d
Andy Price <sixdaysandy@gmail.com>
2022-02-02 09:12:34 +0000

    SP-968 - Add CasRec Migration snapshot and rollback script and runbook #minor (#10)

--- migration-scripts/restore-global-cluster.sh
+++ migration-scripts/restore-global-cluster.sh
@@ -3,18 +3,173 @@
 set -e
 set -o pipefail
 
-# Expected Environment Variables
-# ACCOUNT_NAME=preproduction
-# DATABASE=api
-# ENVIRONMENT_NAME=preproduction
-# PRIMARY_REGION=eu-west-1
-# DR_REGION=eu-west-2
+# SNAPSHOT NAME
+SNAPSHOT_FOR_RESTORE=pre-migration-snapshot-api-202201260915
 
-. common-multi-az.sh
+# Script Configuration
+ACCOUNT_NAME=preproduction
+DATABASE=api
+ENVIRONMENT_NAME=preprodglobal
+PRIMARY_REGION=eu-west-1
+DR_REGION=eu-west-2
+
+
+if [ "$ENVIRONMENT_NAME" = "production" ]; then
+    echo "ERROR - You cannot restore the production database."
+    exit 1
+fi
 
 GLOBAL_CLUSTER=$ENVIRONMENT_NAME-$DATABASE-global
 REGIONAL_CLUSTER=$DATABASE-$ENVIRONMENT_NAME
-SNAPSHOT_FOR_RESTORE=$REGIONAL_CLUSTER-snapshot-for-restore
+
+if [ -z "$DATABASE_VERSION" ]; then
+  DATABASE_VERSION="10.14"
+fi
+
+check_for_and_delete_snapshot() {
+    REGION=$1
+    SNAPSHOT=$2
+    echo "INFO - Checking for existing snapshot"
+    if aws rds describe-db-cluster-snapshots --region $REGION --db-cluster-snapshot-identifier "$SNAPSHOT"; then
+        echo "INFO - Snapshot found, deleting"
+        aws rds delete-db-cluster-snapshot \
+            --region $REGION \
+            --db-cluster-snapshot-identifier "$SNAPSHOT"
+        echo "INFO - Waiting for deletion to complete..."
+        aws rds wait db-cluster-snapshot-deleted \
+            --region $REGION \
+            --db-cluster-snapshot-identifier "$SNAPSHOT"
+        echo "INFO - Snapshot $SNAPSHOT deleted."
+    else
+        echo "INFO - No snapshot found"
+    fi
+}
+
+check_look_up_exists() {
+    LOOKUP=$1
+    if [ "$LOOKUP" = "None" ]
+    then
+        echo "ERROR - Lookup value not found exiting."
+        exit 1
+    fi
+}
+
+check_snapshot_exists() {
+    REGION=$1
+    SNAPSHOT=$2
+    echo "INFO - Checking source snapshot exists"
+    if aws rds describe-db-cluster-snapshots --region $REGION --include-shared --db-cluster-snapshot-identifier "$SNAPSHOT"; then
+        echo "INFO - Snapshot $SNAPSHOT found."
+    else
+        echo "ERROR - Snapshot $SNAPSHOT not found exiting."
+        exit 1
+    fi
+}
+
+wait_for_db_cluster_available() {
+    REGION=$1
+    CLUSTER=$2
+    STATUS=unknown
+    echo "INFO - Waiting for $CLUSTER modification to complete..."
+    while [ "$STATUS" != "available" ]
+    do
+        sleep 60
+        STATUS=$(aws rds describe-db-clusters --region $REGION --db-cluster-identifier $CLUSTER --query=DBClusters[0].Status --output text)
+    done
+    echo "INFO - Modification of $CLUSTER complete"
+}
+
+wait_for_global_cluster_available() {
+    REGION=$1
+    CLUSTER=$2
+    STATUS=unknown
+    echo "INFO - Waiting for $CLUSTER modification to complete..."
+    while [ "$STATUS" != "available" ]
+    do
+        sleep 60
+        STATUS=$(aws rds describe-global-clusters --region $REGION --global-cluster-identifier $CLUSTER --query=GlobalClusters[0].Status --output text)
+    done
+    echo "INFO - Modification of $CLUSTER complete"
+}
+
+wait_for_db_cluster_deleted() {
+    REGION=$1
+    CLUSTER=$2
+    echo "INFO - Waiting for $CLUSTER to be deleted"
+    while aws rds describe-db-clusters --region $REGION --db-cluster-identifier $CLUSTER;
+    do
+        echo "Waiting for Cluster Deletion"
+        sleep 60
+    done
+    echo "INFO - $CLUSTER deleted"
+}
+
+wait_for_global_cluster_deleted() {
+    REGION=$1
+    CLUSTER=$2
+    echo "INFO - Waiting for $CLUSTER to be deleted"
+    while aws rds describe-global-clusters --region $REGION --global-cluster-identifier $CLUSTER;
+    do
+        echo "Waiting for Cluster Deletion"
+        sleep 60
+    done
+    echo "INFO - $CLUSTER deleted"
+}
+
+wait_for_db_instance_available() {
+    REGION=$1
+    INSTANCE=$2
+    RETRIES=${3:-1}
+    RETRY=0
+    echo "INFO - Waiting for $INSTANCE modification to complete..."
+    until [ $RETRY -ge $RETRIES ]
+    do
+        aws rds wait db-instance-available --region $REGION --db-instance-identifier "$INSTANCE" && break
+        echo "INFO - Still waiting for $INSTANCE modification to complete..."
+        RETRY=$[$RETRY+1]
+        sleep 2
+    done
+    echo "INFO - Modification of $INSTANCE complete"
+}
+
+wait_for_db_instance_deleted() {
+    REGION=$1
+    INSTANCE=$2
+    RETRIES=${3:-1}
+    RETRY=0
+    echo "INFO - Waiting for $INSTANCE to be deleted..."
+    until [ $RETRY -ge $RETRIES ]
+    do
+        aws rds wait db-instance-deleted --region $REGION --db-instance-identifier "$INSTANCE" && break
+        echo "INFO - Still waiting for $INSTANCE to be deleted..."
+        RETRY=$[$RETRY+1]
+        sleep 2
+    done
+    echo "INFO - Deletion of $INSTANCE complete"
+}
+
+wait_for_snapshot_completion() {
+    REGION=$1
+    SNAPSHOT=$2
+    RETRIES=${3:-1}
+    echo "INFO - Waiting for snapshot $SNAPSHOT copy to complete..."
+    if
+        RETRY=0
+        until [ $RETRY -ge $RETRIES ]
+        do
+            aws rds wait db-cluster-snapshot-available --region $REGION --db-cluster-snapshot-identifier "$SNAPSHOT" && break
+            echo "INFO - Still waiting for snapshot $SNAPSHOT copy to complete..."
+            RETRY=$[$RETRY+1]
+            sleep 2
+        done
+    then
+        echo "INFO - Snapshot $SNAPSHOT complete"
+    else
+        echo "ERROR - Snapshot $SNAPSHOT failed"
+        exit 1
+    fi
+}
+
 
 # Lookup RDS Password
 RDS_PASSWORD=$(aws secretsmanager get-secret-value \
@@ -97,6 +252,7 @@ echo "INFO - DR_REGION: $DR_REGION"
 echo "INFO - GLOBAL_CLUSTER: $GLOBAL_CLUSTER"
 echo "INFO - REGIONAL_CLUSTER: $REGIONAL_CLUSTER"
 echo "INFO - SNAPSHOT_FOR_RESTORE: $SNAPSHOT_FOR_RESTORE"
+# These are all the derived paramters you need to rerun parts of the script.
 echo "INFO - Cluster Config:-"
 echo "INFO - PRIMARY_CLUSTER_ARN=$PRIMARY_CLUSTER_ARN"
 echo "INFO - DR_CLUSTER_ARN=$DR_CLUSTER_ARN"
@@ -158,14 +314,12 @@ echo "INFO - Removing Deletion Protection $PRIMARY_REGION $REGIONAL_CLUSTER"
 aws rds modify-db-cluster --region $PRIMARY_REGION --db-cluster-identifier $REGIONAL_CLUSTER --no-deletion-protection --apply-immediately
 wait_for_db_cluster_available $PRIMARY_REGION $PRIMARY_CLUSTER_ARN
 
-# Delete $PRIMARY_REGION cluster with final-snapshot
+# Delete $PRIMARY_REGION cluster without final-snapshot
 check_for_and_delete_snapshot $PRIMARY_REGION $REGIONAL_CLUSTER-final-snapshot
 echo "INFO - Deleting Cluster: $PRIMARY_REGION $REGIONAL_CLUSTER"
 aws rds delete-db-cluster --region $PRIMARY_REGION \
     --db-cluster-identifier $REGIONAL_CLUSTER \
-    --no-skip-final-snapshot \
-    --final-db-snapshot-identifier $REGIONAL_CLUSTER-final-snapshot
-wait_for_snapshot_completion $PRIMARY_REGION $REGIONAL_CLUSTER-final-snapshot
+    --skip-final-snapshot
 wait_for_db_cluster_deleted $PRIMARY_REGION $PRIMARY_CLUSTER_ARN
 
 # Restore Cluster from Snapshot
