c1aeaba2cb1888bdc25cd189e3a1fe60a60c2f4b
Joseph Smith <joseph.smith@digital.justice.gov.uk>
2024-04-29 11:15:11 +0100

    SW-7165 Moved new documents script to new file

--- scripts/documents.py
+++ scripts/documents.py
@@ -10,20 +10,127 @@ DESTINATION_ENV = os.environ['DESTINATION_ENV']
 
 credentials = boto3.Session().get_credentials()
 
-DEBUG = True
+DEBUG = False
+
+epa_lpa_query = """
+    SELECT
+    c.uid,
+    c.casetype,
+    CASE
+        WHEN d.filename IN (
+        'CT-bb.pdf',
+        'DD-2.pdf',
+        'EP-10.pdf',
+        'EP-G.pdf',
+        'EP-R.pdf',
+        'EP-REG.pdf',
+        'EP(bb).pdf',
+        'EP-IMP.pdf',
+        'LP-A.pdf',
+        'LP-G1-4.pdf',
+        'LP-G2-4.pdf',
+        'LP-G2D-4.pdf',
+        'LP-INC.pdf',
+        'LP-INV-3A.pdf',
+        'LP-INV-3B.pdf',
+        'LP-J.pdf',
+        'LP-NA-3A.pdf',
+        'LP-NB-3B.pdf',
+        'LP-R.pdf',
+        'LP1F.pdf'
+        )
+        THEN CONCAT(CAST(d.id AS TEXT),'_',d.filename)
+        ELSE d.filename
+    END as s3_name
+    FROM
+    cases c
+    INNER JOIN persons p
+    ON c.donor_id = p.id
+    INNER JOIN caseitem_document cid
+    ON c.id = cid.caseitem_id
+    	INNER JOIN documents d
+    	ON cid.document_id = d.id
+    WHERE
+    c.uid = {case}
+    UNION ALL
+    SELECT
+    c.uid,
+    c.casetype,
+    d.filename
+    FROM
+    cases c
+    INNER JOIN persons p
+    ON c.donor_id = p.id
+    INNER JOIN person_note pn
+    ON p.id = pn.person_id
+    INNER JOIN notes n
+    ON pn.note_id = n.id
+    INNER JOIN documents d
+    ON n.document_id = d.id
+    WHERE
+    c.uid = {case}
+    UNION ALL
+    SELECT
+    c.uid,
+    c.casetype,
+    d.filename
+    FROM
+    cases c
+    INNER JOIN persons p
+    ON c.donor_id = p.id
+    INNER JOIN person_document pd
+    ON p.id = pd.person_id
+    INNER JOIN documents d
+    ON pd.document_id = d.id
+    WHERE
+    c.uid = {case};
+    """
 
 supervision_query = """
     SELECT
-    documents.filename
-    FROM documents
-    LEFT JOIN caseitem_document cd ON documents.id = cd.document_id
-    LEFT JOIN cases ON cd.caseitem_id = cases.id
-    LEFT JOIN persons clients ON cases.client_id = clients.id
-    LEFT JOIN persons feepayers ON clients.feepayer_id = feepayers.id
-    WHERE CAST(publisheddate AS DATE) >= '2024-04-19'
-    AND (feepayers.email = '' OR feepayers.email IS NULL)
-    AND systemtype = 'af2';
+    c.uid,
+    c.casetype,
+    d.filename
+    FROM
+    cases c
+    INNER JOIN persons p
+    ON c.client_id = p.id
+    INNER JOIN caseitem_document cid
+    ON c.id = cid.caseitem_id
+    	INNER JOIN documents d
+    	ON cid.document_id = d.id
+    WHERE
+    c.uid = {case}
+    AND
+    d.content IS NULL
+    UNION ALL
+    SELECT
+    c.uid,
+    c.casetype,
+    d.filename
+    FROM
+    cases c
+    INNER JOIN persons p
+    ON c.client_id = p.id
+    	INNER JOIN person_document pd
+    	ON p.id = pd.person_id
+    		INNER JOIN documents d
+    		ON pd.document_id = d.id
+    WHERE c.uid = {case}
+    AND
+    d.content IS NULL;
     """
+
+clients_query = """
+SELECT
+    p.caserecnumber,
+    p.type,
+    d.filename
+FROM persons p
+    INNER JOIN person_document pd ON p.id = pd.person_id
+    INNER JOIN documents d ON pd.document_id = d.id
+WHERE p.caserecnumber = '{case}';
+"""
 
 
 def describe_pg_host(environment):
@@ -81,22 +188,24 @@ def copy_document(src_bucket_name, dest_bucket_name, document_name):
     return True
 
 
-def get_documents(query):
-    cursor.execute(query)
+def get_documents(query, case):
+    cursor.execute(query.format(case=case))
     documents = cursor.fetchall()
     for document in documents:
         if DEBUG:
             print(
-                "Document Storage Name = {doc_name}".format(
-                    doc_name=document[0]
+                "Case Reference = {case_ref} Case Type = {case_type}, Document Storage Name = {doc_name}".format(
+                    case_ref=document[0],
+                    case_type=document[1],
+                    doc_name=document[2]
                 )
             )
         else:
             copy_document(
                 'opg-backoffice-datastore-'+SOURCE_ENV,
                 'opg-backoffice-datastore-'+DESTINATION_ENV,
-                document[0])
-            print('document ' + document[0] + ' copied to destination bucket')
+                document[2])
+            print('document ' + document[2] + ' copied to destination bucket')
 
 
 PG_DATABASE = describe_pg_database(DESTINATION_ENV)
@@ -113,6 +222,41 @@ connection = psycopg2.connect(
 )
 print("Database opened successfully")
 
-get_documents(supervision_query, case_ref)
+# Open CSV of Test Cases
+with open('cases.csv', newline='') as file:
+    contents = csv.reader(file)
+    ids = list(contents)
+    cases = []
+    clients = []
+    for id in ids:
+        if re.search(r'\d{4}-\d{4}-\d{4}', str(id)):
+            cases.append(id)
+            if DEBUG:
+                print("Appended to cases: ", id)
+        else:
+            clients.append(id)
+            if DEBUG:
+                print("Appended to clients: ", id)
+
+for case in cases:
+    case = case[0].replace("-", "")
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT uid, casetype FROM cases WHERE uid = {case}".format(case=case))
+    case = cursor.fetchall()
+    case_ref = str(case[0][0])
+    case_type = case[0][1]
+    # Check if case is LPA or ORDER
+    if (case_type == "LPA") or (case_type == "EPA"):
+        print("Getting {case_type} Docs for:".format(
+            case_type=case_type), case_ref)
+        get_documents(epa_lpa_query, case_ref)
+    if case_type == "ORDER":
+        print("Getting ORDER Docs for:", case_ref)
+        get_documents(supervision_query, case_ref)
+
+for client in clients:
+    cursor = connection.cursor()
+    get_documents(clients_query, str(client[0]))
 
 print("Operation done successfully")
