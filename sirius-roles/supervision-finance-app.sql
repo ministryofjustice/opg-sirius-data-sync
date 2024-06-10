1bce4f7d74f2a6de4a55370867c3b7680579831e
Andy Price <sixdaysandy@gmail.com>
2024-06-10 09:06:25 +0100

    SP-2174 - Create App Specific PostgreSQL Roles for Apps #minor

---
+++ sirius-roles/supervision-finance-app.sql
@@ -0,0 +1,9 @@
+GRANT CONNECT ON DATABASE :"database_name" TO :"user_name";
+
+# supervision_finance schema read/write acces
+GRANT USAGE ON SCHEMA supervision_finance TO :"user_name";
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA supervision_finance TO :"user_name";
+GRANT USAGE, SELECT ON SEQUENCES TO :"user_name";
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA supervision_finance TO :"user_name";
+ALTER DEFAULT PRIVILEGES IN SCHEMA supervision_finance GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO :"user_name";
+ALTER DEFAULT PRIVILEGES IN SCHEMA supervision_finance GRANT USAGE, SELECT ON SEQUENCES TO :"user_name";
