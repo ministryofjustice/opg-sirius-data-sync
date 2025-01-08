516423e8085b6aff181541f4c7fb244d22919ccd
Sam Joseph <denialanderror@users.noreply.github.com>
2025-01-08 14:02:14 +0000

    Adds public read access to Finance App. Finance Admin will be removed in a subsequent PR. (#73)

--- sirius-roles/supervision-finance-app.sql
+++ sirius-roles/supervision-finance-app.sql
@@ -1,8 +1,13 @@
 GRANT CONNECT ON DATABASE :"database_name" TO :"user_name";
 
-/* supervision_finance schema read/write acces */
+/* supervision_finance schema read/write access */
 GRANT USAGE ON SCHEMA supervision_finance TO :"user_name";
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA supervision_finance TO :"user_name";
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA supervision_finance TO :"user_name";
 ALTER DEFAULT PRIVILEGES IN SCHEMA supervision_finance GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO :"user_name";
 ALTER DEFAULT PRIVILEGES IN SCHEMA supervision_finance GRANT USAGE, SELECT ON SEQUENCES TO :"user_name";
+
+/* public schema read access */
+GRANT USAGE ON SCHEMA public TO :"user_name";
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO :"user_name";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO :"user_name";
