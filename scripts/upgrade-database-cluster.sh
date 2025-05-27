fbf515b66c68ff36edc824cbf0d25fbe69b799fd
Andy Price <sixdaysandy@gmail.com>
2025-05-27 14:16:18 +0100

    SP-2829 - Add Script to manage databse cluster upgrade #minor

---
+++ scripts/upgrade-database-cluster.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [ -z "$ENVIRONMENT_NAME" ]; then
+    echo "ERROR - You need to set the ENVIRONMENT_NAME environment variable."
+    exit 1
+fi
+
+if [ "$ENVIRONMENT_NAME" = "production" || "$ENVIRONMENT_NAME" = "preproduction" || "$ENVIRONMENT_NAME" = "dev" ]; then
+    echo "ERROR - You cannot upgrade the $ENVIRONMENT_NAME global database cluster."
+    exit 1
+fi
+
+if [ -z "$DATABASE" ]; then
+    echo "ERROR - You need to set the DATABASE environment variable."
+    exit 1
+fi
+
+if [ -z "$TARGET_VERSION" ]; then
+    echo "ERROR - You need to set the TARGET_VERSION environment variable."
+    exit 1
+fi
+
+
+DATABASE_CLUSTER=$DATABASE-$ENVIRONMENT_NAME
+echo "INFO - DATABASE_CLUSTER set to $DATABASE_CLUSTER"
+echo "INFO - Database Target Version set to $TARGET_VERSION"
+echo "INFO - Upgrading Database Cluster $DATABASE_CLUSTER to PostgreSQL $TARGET_VERSION"
+
+aws rds modify-db-cluster --db-cluster-identifier $DATABASE_CLUSTER \
+    --allow-major-version-upgrade \
+    --engine-version $TARGET_VERSION \
+    --db-cluster-parameter-group-name aurora-postgresql$TARGET_VERSION-ssl \
+    --db-instance-parameter-group-name default.aurora-postgresql$TARGET_VERSION \
+    --apply-immediately
+
+wait_for_db_cluster_available $DATABASE_CLUSTER
+echo "INFO - Database Upgrade Complete"
