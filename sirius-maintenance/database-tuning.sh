f39ccfdd7756a76da08294ab020bcc5d5a20bbb6
Andy Price <sixdaysandy@gmail.com>
2025-08-04 13:56:40 +0100

    SP-2974 - Parallelise Database Tuning #minor

--- sirius-maintenance/database-tuning.sh
+++ sirius-maintenance/database-tuning.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-psql -U $PGUSER --dbname=$DATABASE_NAME --command='VACUUM VERBOSE ANALYSE;'
+psql -U $PGUSER --dbname=$DATABASE_NAME --command='VACUUM (PARALLEL 8, VERBOSE, ANALYSE);'
