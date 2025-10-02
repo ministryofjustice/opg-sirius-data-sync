c9506357d38e58e6c53c5f7d0ecac84ade79e67e
Andy Price <sixdaysandy@gmail.com>
2025-10-02 13:17:28 +0100

    SP-3175 - Remove operator schema access #minor

--- sirius-roles/operator.sql
+++ sirius-roles/operator.sql
@@ -3,26 +3,6 @@ GRANT CONNECT ON DATABASE :"database_name" TO :"user_name";
 /* log statement usage for audit */
 ALTER ROLE :"user_name" SET log_statement="all";
 
-/* public schema read acces */
-GRANT USAGE ON SCHEMA public TO :"user_name";
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO :"user_name";
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO :"user_name";
-
-/* poa schema read acces */
-GRANT USAGE ON SCHEMA poa TO :"user_name";
-GRANT SELECT ON ALL TABLES IN SCHEMA poa TO :"user_name";
-ALTER DEFAULT PRIVILEGES IN SCHEMA poa GRANT SELECT ON TABLES TO :"user_name";
-
-/* supervision schema read acces */
-GRANT USAGE ON SCHEMA supervision TO :"user_name";
-GRANT SELECT ON ALL TABLES IN SCHEMA supervision TO :"user_name";
-ALTER DEFAULT PRIVILEGES IN SCHEMA supervision GRANT SELECT ON TABLES TO :"user_name";
-
-/* supervision_finance schema read acces */
-GRANT USAGE ON SCHEMA supervision_finance TO :"user_name";
-GRANT SELECT ON ALL TABLES IN SCHEMA supervision_finance TO :"user_name";
-ALTER DEFAULT PRIVILEGES IN SCHEMA supervision_finance GRANT SELECT ON TABLES TO :"user_name";
-
 /* add operator to groups to allow viewing of running query stats and rogue query management */
 GRANT pg_read_all_stats TO :"user_name";
 GRANT pg_signal_backend TO :"user_name";
