b3d58458ec0a8a55bab492c362d1ae1062e34303
Andy Price <sixdaysandy@gmail.com>
2025-10-03 08:03:00 +0100

    SP-3175 - Role Fixes #minor

--- sirius-roles/operator.sql
+++ sirius-roles/operator.sql
@@ -3,6 +3,12 @@ GRANT CONNECT ON DATABASE :"database_name" TO :"user_name";
 /* log statement usage for audit */
 ALTER ROLE :"user_name" SET log_statement="all";
 
+/* Revoke previous permissions */
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM :"user_name";
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA poa FROM :"user_name";
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA supervision FROM :"user_name";
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA supervision_finance FROM :"user_name";
+
 /* add operator to groups to allow viewing of running query stats and rogue query management */
 GRANT pg_read_all_stats TO :"user_name";
 GRANT pg_signal_backend TO :"user_name";
--- test/setup_sirius_db.sql
+++ test/setup_sirius_db.sql
@@ -2,6 +2,7 @@ CREATE DATABASE api;
 
 \c api
 
+CREATE SCHEMA analytics_export;
 CREATE SCHEMA poa;
 CREATE SCHEMA supervision;
 CREATE SCHEMA supervision_finance;
