43692005a247e38be505898c714611f4fac6766a
AidanJ97 <aidan.johnson@digital.justice.gov.uk>
2025-10-08 15:59:16 +0100

    SP-2944 - set up db role #minor

---
+++ scripts/suspend-nologin-assignees.py
@@ -0,0 +1,71 @@
+
+import boto3
+import logging
+import os
+import psycopg
+
+LOGGER = logging.getLogger()
+logging.basicConfig()
+LOGGER.setLevel(logging.INFO)
+
+ASSIGNEES_ACCESS_PASSWORD = os.environ.get('ASSIGNEES_ACCESS_PASSWORD')
+DYNAMO_TABLE = os.environ.get('DYNAMO_TABLE')
+RDS_DB_NAME = os.environ.get('RDS_DB_NAME')
+PG_HOST = os.environ.get('PG_HOST')
+PG_PORT = os.environ.get('PG_PORT')
+
+LOGGER.info('Script starting')
+DYNAMO_CLIENT = boto3.client('dynamodb')
+
+def getAllUsers():
+    scan_args = {
+        'TableName': DYNAMO_TABLE,
+        'ProjectionExpression': 'email'
+    }
+
+    items = []
+    last_evaluated_key = None
+    while True:
+        if last_evaluated_key:
+            scan_args['ExclusiveStartKey'] = last_evaluated_key
+        response = DYNAMO_CLIENT.scan(**scan_args)
+        items.extend(response.get('Items', []))
+
+        last_evaluated_key = response.get('LastEvaluatedKey')
+        if not last_evaluated_key:
+            break
+
+    return items
+
+def suspendNologinAssignees(activeEmails):
+    LOGGER.info("Connecting to DB...")
+    with psycopg.connect(
+        dbname=RDS_DB_NAME,
+        user='assignees-access',
+        password=ASSIGNEES_ACCESS_PASSWORD,
+        host=PG_HOST,
+        port=PG_PORT
+    ) as connection:
+        
+        LOGGER.info('Suspending assignees never logged in...')
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT email FROM assignees WHERE suspended = FALSE AND email IS NOT NULL AND type = 'assignee_user';")
+            unsuspended_assignees = [row[0] for row in cursor.fetchall()]
+            
+            nologin_emails = list(set(unsuspended_assignees) - set(activeEmails))
+            
+            if nologin_emails:
+                query = "UPDATE assignees SET suspended = TRUE WHERE email = ANY(%s);"
+                cursor.execute(query, (nologin_emails,))
+                LOGGER.info(f"Suspended {len(nologin_emails)} assignees never logged in.")
+            else:
+                LOGGER.info("No assignees never logged in found.")
+
+LOGGER.info("Getting all users from DynamoDB...")
+allUsers = getAllUsers()
+
+if not allUsers:
+    LOGGER.warning("No users found in DynamoDB.")
+else:
+    activeEmails = [user['email']['S'] for user in allUsers]
+    suspendNologinAssignees(activeEmails)
