27e866307b93b0b23c6a04143cd29507b844923b
AidanJ97 <aidan.johnson@digital.justice.gov.uk>
2025-10-08 15:59:16 +0100

    SP-2944 - set up db role #minor

---
+++ scripts/suspend-inactive-assignees.py
@@ -0,0 +1,109 @@
+# from botocore.config import Config
+import boto3
+import logging
+import os
+import psycopg2
+import datetime
+from dateutil.relativedelta import relativedelta  
+
+expire_date = (datetime.datetime.now().date() - relativedelta(months=3)).strftime("%Y-%m-%d")
+
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.INFO)
+
+# account_id = os.environ.get('ACCOUNT_ID')
+# environment_name = os.environ.get('ENVIRONMENT_NAME')
+# region_name = os.environ.get('AWS_REGION', 'eu-west-1')
+assignees_access_password = os.environ.get('ASSIGNEES_ACCESS_PASSWORD')
+dynamo_table = os.environ.get('DYNAMO_TABLE')
+rds_db_name = os.environ.get('RDS_DB_NAME')
+pg_host = os.environ.get('PG_HOST')
+pg_port = os.environ.get('PG_PORT')
+
+logger.info('Script starting')
+dynamoClient = boto3.client('dynamodb')
+
+def getExpiredUsers(client, dynamo_table, expire_date):
+    scan_args = {
+        'TableName': dynamo_table,
+        'ExpressionAttributeNames': {
+            "#email": "email",
+            "#time": "time"
+        },
+        'ExpressionAttributeValues': {
+            ':a': {'S': expire_date}
+        },
+        'FilterExpression': '#time < :a',
+        'ProjectionExpression': '#email'
+    }
+    items = []
+    last_evaluated_key = None
+
+    while True:
+        if last_evaluated_key:
+            scan_args['ExclusiveStartKey'] = last_evaluated_key
+        
+        response = client.scan(**scan_args)
+        items.extend(response.get('Items', []))
+
+        last_evaluated_key = response.get('LastEvaluatedKey')
+
+        if not last_evaluated_key:
+            break
+
+    return items
+
+def deleteExpiredUsers(client, items=[]):
+    batchSize = 2
+
+    for i in range(0, len(items), batchSize):
+        batch_items = items[i:i + batchSize]
+        request_items = {
+            dynamo_table: [
+                {'DeleteRequest': {'Items': item}} for item in batch_items
+            ]
+        }
+
+        client.batchWriteItem(RequestItems=request_items)
+
+        logger.info(request_items)
+        logger.info(f"deleted batch number {i+1}")
+
+
+
+# def getAllUsers(client):
+#     response = client.scan(
+#         TableName=dynamo_table,
+#     )
+#     items = response['Items']
+
+#     while('LastEvaluatedKey' in response):
+#         response = client.scan(
+#             TableName=dynamo_table,
+#             ExclusiveStartKey=response['LastEvaluatedKey']
+#         )
+#         items.append(response['Items'])
+    
+#     return items
+
+
+expiredUsers = getExpiredUsers(dynamoClient)
+
+logger.info(expiredUsers)
+
+# connection = psycopg2.connect(
+#     database=rds_db_name,
+#     user='assignees-access',
+#     password=assignees_access_password,
+#     host=pg_host,
+#     port=pg_port
+# )
+
+# cursor = connection.cursor()
+# cursor.execute("SELECT * FROM assignees;")
+
+# print(cursor.fetchall())
+
+# cursor.close()
+# connection.close()
