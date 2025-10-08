9a09fddde8668d662b879aabca18b24184811f89
AidanJ97 <aidan.johnson@digital.justice.gov.uk>
2025-10-08 15:59:16 +0100

    SP-2944 - set up db role #minor

---
+++ scripts/suspend-inactive-assignees.py
@@ -0,0 +1,21 @@
+# from botocore.config import Config
+import boto3
+import logging
+import os
+
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.INFO)
+
+# account_id = os.environ.get('ACCOUNT_ID')
+table_name = os.environ.get('TABLE_NAME')
+# environment_name = os.environ.get('ENVIRONMENT_NAME')
+# region_name = os.environ.get('AWS_REGION', 'eu-west-1')
+
+logger.info('Script starting')
+
+dynamo = boto3.client('dynamodb')
+
+response = dynamo.scan(TableName=table_name)
+
+logger.info(response)
