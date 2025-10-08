0dadbd8e287823a927e4019dc65e9d5447cc229b
AidanJ97 <aidan.johnson@digital.justice.gov.uk>
2025-10-08 15:59:16 +0100

    SP-2944 - set up db role #minor

---
+++ scripts/suspend-inactive-assignees.py
@@ -0,0 +1,27 @@
+# from botocore.config import Config
+import boto3
+import logging
+import os
+
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.INFO)
+
+# account_id = os.environ.get('ACCOUNT_ID')
+# database_name = os.environ.get('DATABASE_NAME')
+# environment_name = os.environ.get('ENVIRONMENT_NAME')
+region_name = os.environ.get('AWS_REGION', 'eu-west-1')
+
+logger.info('Script starting')
+
+# dynamo_config = Config(
+#         region_name = region_name,
+#     )
+
+dynamo = boto3.client('dynamodb')
+
+response = dynamo.list_tables(
+    Limit=15
+)
+
+logger.info(response)
---
+++ sirius-roles/assignees-access.sql
@@ -0,0 +1,9 @@
+GRANT CONNECT ON DATABASE :"database_name" TO :"user_name";
+
+/* log statement usage for audit */
+ALTER ROLE :"user_name" SET log_statement="all";
+
+/* assignees schema read/update access */
+GRANT USAGE ON SCHEMA public TO :"user_name";
+GRANT SELECT, UPDATE ON assignees IN SCHEMA public TO :"user_name";
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, UPDATE ON assignees TO :"user_name";
