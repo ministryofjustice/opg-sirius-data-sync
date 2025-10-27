53ed1743527e1a9d4a68fdf4aeeaf66fbed728e4
Aidan Johnson <37669925+AidanJ97@users.noreply.github.com>
2025-10-27 15:28:38 +0000

    SP-2994 - suspend inactive assignees #minor (#105)
    * SP-2944 - set up db role #minor

--- sirius-roles/search-app.sql
+++ sirius-roles/search-app.sql
@@ -1,11 +1,11 @@
 GRANT CONNECT ON DATABASE :"database_name" TO :"user_name";
 
-/* public schema read acces */
+/* public schema read access */
 GRANT USAGE ON SCHEMA public TO :"user_name";
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO :"user_name";
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO :"user_name";
 
-/* supervision schema read acces */
+/* supervision schema read access */
 GRANT USAGE ON SCHEMA supervision TO :"user_name";
 GRANT SELECT ON ALL TABLES IN SCHEMA supervision TO :"user_name";
 ALTER DEFAULT PRIVILEGES IN SCHEMA supervision GRANT SELECT ON TABLES TO :"user_name";
