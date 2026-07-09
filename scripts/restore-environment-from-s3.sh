04b763907181ff4f020c620729b5e020d28226d9
Aidan Johnson <37669925+AidanJ97@users.noreply.github.com>
2026-07-09 16:27:06 +0100

    SP-3881 - copy backup straight into s3 #minor (#156)
    * SP-3881 - copy backup straight into s3 #minor
    
    * SP-3881 - restrore straight from s3 #minor

--- scripts/restore-environment-from-s3.sh
+++ scripts/restore-environment-from-s3.sh
@@ -18,7 +18,7 @@ if [ "$RESTORE_FROM_TRAINING_DATA" == "true" ]; then
     export SOURCE_ENVIRONMENT=training-data
 elif [ -n "$CUSTOM_ENV_TO_RESTORE_FROM" ]; then
     export SOURCE_ENVIRONMENT=$CUSTOM_ENV_TO_RESTORE_FROM
-else 
+else
     export SOURCE_ENVIRONMENT=$ENVIRONMENT
 fi
 echo "INFO - Restoring from $SOURCE_ENVIRONMENT"
@@ -30,16 +30,11 @@ function  restore_database_from_s3() {
     local db_name=$1
     local target_env=${ENVIRONMENT}
     local source_env=${SOURCE_ENVIRONMENT}
-    echo "INFO - Downloading from s3://${BUCKET}/database/${source_env}-${db_name}-backup.sql"
-    aws s3 cp s3://${BUCKET}/database/${source_env}-${db_name}-backup.sql ${target_env}-${db_name}-backup.sql
-    echo "INFO - Download Complete"
-
     local db_info=$(aws rds describe-db-clusters --db-cluster-identifier ${db_name}-${target_env})
     export PGDATABASE=$(echo $db_info | jq -r '.DBClusters[].DatabaseName')
     export PGUSER=$(echo $db_info | jq -r '.DBClusters[].MasterUsername')
     export PGHOST=$(echo $db_info | jq -r '.DBClusters[].Endpoint')
     export PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id rds-${db_name}-development | jq -r '.SecretString')
-    echo "INFO - Restoring local file ${target_env}-${db_name}-backup.sql to ${db_name}"
     echo "INFO - Dropping & Recreating poa Schema"
     psql -c 'DROP SCHEMA poa CASCADE;'
     psql -c 'CREATE SCHEMA poa;'
@@ -52,7 +47,8 @@ function  restore_database_from_s3() {
     echo "INFO - Dropping & Recreating supervision_finance Schema"
     psql -c 'DROP SCHEMA supervision_finance CASCADE;'
     psql -c 'CREATE SCHEMA supervision_finance;'
-    psql -f ${target_env}-${db_name}-backup.sql
+    echo "INFO - Restoring s3://${BUCKET}/database/${source_env}-${db_name}-backup.sql to ${db_name}"
+    aws s3 cp s3://${BUCKET}/database/${source_env}-${db_name}-backup.sql - | psql
     echo "INFO - Restore complete."
 }
 
