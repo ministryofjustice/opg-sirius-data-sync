7e5d2731f86a32644512a690680ba28ded67a359
Andy Price <sixdaysandy@gmail.com>
2026-08-26 11:34:03 +0100

    SP-4023 - Switch from Inspec to Goss for Testing #minor

---
+++ scripts/s3-copy-wrapper.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [ -z "$SOURCE_PATH" ]; then
+    echo "ERROR - You need to set the SOURCE_PATH environment variable."
+    exit 1
+fi
+
+if [ -z "$DESTINATION_PATH" ]; then
+    echo "ERROR - You need to set the DESTINATION_PATH environment variable."
+    exit 1
+fi
+
+if [ -z "$COMMAND" ]; then
+    echo "ERROR - You need to set the COMMAND environment variable."
+    exit 1
+fi
+
+if [ "$COMMAND" != "cp" ] && [ "$COMMAND" != "sync" ]; then
+    echo "ERROR - COMMAND must be one of 'cp' or 'sync'."
+    exit 1
+fi
+
+if [ -z "$DRY_RUN" ]; then
+    DRY_RUN_FLAG=""
+else
+    DRY_RUN_FLAG="--dryrun"
+    echo "INFO - Dry Run Flag set, no data will be transferred"
+fi
+
+echo "INFO - $COMMAND-ing $SOURCE_PATH to $DESTINATION_PATH"
+aws s3 $COMMAND $SOURCE_PATH $DESTINATION_PATH --sse $DRY_RUN_FLAG
+echo "INFO - $COMMAND Complete"
